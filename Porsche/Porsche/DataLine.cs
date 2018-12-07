4720785a1622019f13540649472fa48628b0d806
Charlie179 <1669247240@qq.com>
2018-12-07 22:17:03 +0800

    Add the new repository

---
+++ Porsche/Porsche/DataLine.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Porsche
+{
+    /// <summary>
+    /// 数据行
+    /// </summary>
+    public class DataLine
+    {
+        /// <summary>
+        /// 数据行类型
+        /// </summary>
+        public enum DataType
+        {
+            /// <summary>
+            /// 错误
+            /// </summary>
+            Error,
+            /// <summary>
+            /// 标识
+            /// </summary>
+            Iden,
+            /// <summary>
+            /// 类型
+            /// </summary>
+            Type,
+            /// <summary>
+            /// 数值
+            /// </summary>
+            Value,
+        }
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public DataType Type { get; private set; }
+
+        /// <summary>
+        /// 列
+        /// </summary>
+        public string[] Columns { get; private set; }
+
+        /// <summary>
+        /// 该行内容([]中间的字符串)
+        /// </summary>
+        private string content;
+
+        /// <summary>
+        /// 实例化DataLine
+        /// </summary>
+        public DataLine(string str, int columnNumber)
+        {
+            this.Id = 0;
+            this.Type = DataType.Error;
+            this.Columns = null;
+            this.content = str.Substring(1, str.Length - 2); //去除掉[]
+            this.Columns = Compile(columnNumber);              //编译
+        }
+
+        /// <summary>
+        /// 编译该行
+        /// </summary>
+        private string[] Compile(int columnNumber)
+        {
+            List<string> list;
+            string[] array = new string[columnNumber];
+
+            list = CheckIdenLine();
+            if (list != null)
+            {
+                //如果指定列数量超过集合长度, 返回集合
+                if (list.Count < columnNumber)
+                    return list.ToArray();
+
+                list.CopyTo(0, array, 0, (int)columnNumber);
+                return array;
+            }
+
+            list = CheckTypeLine();
+            if (list != null)
+            {
+                //如果指定列数量超过集合长度, 返回集合
+                if (list.Count < columnNumber)
+                    return list.ToArray();
+
+                list.CopyTo(0, array, 0, (int)columnNumber);
+                return array;
+            }
+
+            list = CheckValueLine();
+            if (list != null)
+            {
+                //如果指定列数量超过集合长度, 返回集合
+                if (list.Count < columnNumber)
+                    return list.ToArray();
+
+                list.CopyTo(0, array, 0, (int)columnNumber);
+                return array;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 检查是否是类型行
+        /// </summary>
+        private List<string> CheckTypeLine()
+        {
+            //如果不是类型行
+            if (content.IndexOf('~') == -1)
+                return null;
+            //如果是标识行
+            if (content.IndexOf('@') != -1)
+                return null;
+            //如果是数值行
+            if (content.IndexOf('#') != -1)
+                return null;
+
+            List<string> list = new List<string>();
+
+            //类型关键字
+            string[] keyword = new string[] { "string", "int", "float", "bool" };
+
+            //根据,拆分字符串
+            string[] types = content.Split(',');
+            //遍历列
+            for (int i = 0; i < types.Length; i++)
+            {
+                //去除空格
+                types[i] = types[i].Trim();
+                //检测 ~ 符号
+                if (types[i].First() == '~')
+                {
+                    //去除掉 ~ 符号
+                    types[i] = types[i].Substring(1);
+                    //遍历每一个关键字
+                    for (int j = 0; j < keyword.Length; j++)
+                    {
+                        //匹配上
+                        if (types[i] == keyword[j])
+                        {
+                            list.Add(types[i]);
+                        }
+                    }
+                }
+            }
+            //如果列中有无关的则返回空
+            if (list.Count != types.Length)
+                return null;
+
+            Type = DataType.Type;
+            return list;
+        }
+
+        /// <summary>
+        /// 检查是否是标识行
+        /// </summary>
+        private List<string> CheckIdenLine()
+        {
+            //不是标识行
+            if (content.IndexOf('@') == -1)
+                return null;
+            //是类型行
+            if (content.IndexOf('~') != -1)
+                return null;
+            //是数值行
+            if (content.IndexOf('#') != -1)
+                return null;
+
+            Type = DataType.Iden;
+
+            List<string> list = new List<string>();
+
+            string[] idens = content.Split(',');
+            for (int i = 0; i < idens.Length; i++)
+            {
+                //去除空格
+                idens[i] = idens[i].Trim();
+                //检测 @ 符号
+                if (idens[i].First() == '@')
+                {
+                    idens[i] = idens[i].Substring(1); //去除@符号
+                    list.Add(idens[i]);
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 检查是否是数值行
+        /// </summary>
+        private List<string> CheckValueLine()
+        {
+            //不是数值行
+            if (content.IndexOf('#') == -1)
+                return null;
+            //是类型行
+            if (content.IndexOf('~') != -1)
+                return null;
+            //是标识行
+            if (content.IndexOf('@') != -1)
+                return null;
+
+            Type = DataType.Value;
+
+            List<string> list = new List<string>();
+
+            string[] values = content.Split(',');
+            for (int i = 0; i < values.Length; i++)
+            {
+                //去除空格
+                values[i] = values[i].Trim();
+                //检测 # 符号
+                if (values[i].First() == '#')
+                {
+                    values[i] = values[i].Substring(1); //去除#符号
+                    list.Add(values[i]);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ Porsche/Porsche/DataTable.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Porsche
+{
+    /// <summary>
+    /// 数据表
+    /// </summary>
+    public class DataTable
+    {
+        /// <summary>
+        /// 下标
+        /// </summary>
+        private int dataLineIndex;
+
+        /// <summary>
+        /// 行数
+        /// </summary>
+        public int LineNumber
+        {
+            get
+            {
+                if (AllLines != null)
+                    return AllLines.Count;
+                else
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// 列数
+        /// </summary>
+        public int ColumnNumber
+        {
+            get
+            {
+                if (AllLines != null && AllLines.Count > 0 && AllLines[0].Columns != null)
+                    return AllLines[0].Columns.Length;
+                else
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// 所有行
+        /// </summary>
+        private List<DataLine> AllLines;
+
+        /// <summary>
+        /// 标识行
+        /// </summary>
+        public DataLine IdenLine;
+
+        /// <summary>
+        /// 类型行
+        /// </summary>
+        public DataLine TypeLine;
+
+        /// <summary>
+        /// 所有数据行
+        /// </summary>
+        public List<DataLine> ValueLines;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public DataTable(int valueLineIndex, List<DataLine> allLines)
+        {
+            this.dataLineIndex = valueLineIndex;
+
+            this.AllLines = allLines;
+            this.ValueLines = new List<DataLine>();
+        }
+
+        /// <summary>
+        /// 创建数据表
+        /// </summary>
+        public bool Creat()
+        {
+            if (AllLines == null)
+                return false;
+            //遍历数据行
+            for (int i = 0; i < this.AllLines.Count; i++)
+            {
+                //检测类型
+                switch (this.AllLines[i].Type)
+                {
+                    case DataLine.DataType.Iden:
+                        this.IdenLine = this.AllLines[i];
+                        break;
+                    case DataLine.DataType.Type:
+                        this.TypeLine = this.AllLines[i];
+                        break;
+                    case DataLine.DataType.Value:
+                        {
+                            //设置Id(必须借助中间变量)
+                            var temp = this.AllLines[i];
+                            temp.Id = this.dataLineIndex++; //先赋值后+1
+                            this.AllLines[i] = temp;
+                            //Id只作用于数据行
+                            this.ValueLines.Add(this.AllLines[i]);
+                        }
+                        break;
+                }
+            }
+            //检查数据表
+            if (this.IdenLine == null)
+            {
+                Console.WriteLine("没有标识行!");
+                return false;
+            }
+            if (this.TypeLine == null)
+            {
+                Console.WriteLine("没有类型行!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
