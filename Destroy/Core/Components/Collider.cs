b277f554e48d9f5bd6bc7be5f82084b6ffbae294
kyasever <kyasever@163.com>
2018-12-08 18:05:36 +0800

    Physices

--- Destroy/Core/Components/Collider.cs
+++ Destroy/Core/Components/Collider.cs
@@ -1,7 +1,215 @@
-﻿namespace Destroy
+﻿using System.Collections.Generic;
+
+namespace Destroy
 {
-    public class Collider : Component
+    /*
+    * 12/8 by Kyasever
+    * RigidBody 控制移动和碰撞
+    * Collider应该分为两个组件
+    * MeshCollider 自动检测Renderer组件并自动生成Collider
+    * Static 初始化的时候就存在. 只要标记为static,就永远都在
+    * 之后版本要重新优化
+    */
+    public abstract class Collider : Component
     {
         public Collider() { }
+    }
+
+    /// <summary>
+    /// 静态碰撞体,包含这个组件的对象只能在游戏开始的时候创建
+    /// 会进行缓冲处理,提升系统性能.
+    /// </summary>
+    public class StaticCollider: Collider
+    {
+        public List<Vector2Int> posList;
+        public StaticCollider()
+        {
+            posList = new List<Vector2Int>();
+            posList.Add(new Vector2Int(0, 0));
+        }
+        /// <summary>
+        /// 直接初始化Collider
+        /// </summary>
+        public void Init(List<Vector2Int> l)
+        {
+            posList = l;
+        }
+        /// <summary>
+        /// 通过Renderer初始化Collider
+        /// </summary>
+        public void InitWithRenderer(Renderer r)
+        {
+            //添加当前点
+            if (r.GetType() == typeof(PosRenderer))
+            {
+                posList.Add(new Vector2Int(0, 0));
+            }
+            //添加一排碰撞体
+            else if (r.GetType() == typeof(StringRenderer))
+            {
+                int n = 0;
+                for (int i = 0; i < ((StringRenderer)r).length; i += Camera.main.CharWidth)
+                {
+                    posList.Add(new Vector2Int(n, 0));
+                    n++;
+                }
+            }
+            //添加组碰撞体
+            else if (r.GetType() == typeof(GroupRenderer))
+            {
+                GroupRenderer gr = r as GroupRenderer;
+                foreach (var v in gr.list)
+                {
+                    if (v.Key.GetType() == typeof(PosRenderer))
+                    {
+                        posList.Add(v.Value);
+                    }
+                    else if(v.Key.GetType() == typeof(StringRenderer))
+                    {
+                        int n = 0;
+                        for (int i = 0; i < ((StringRenderer)v.Key).length; i += Camera.main.CharWidth)
+                        {
+                            posList.Add(new Vector2Int(n, 0) + v.Value);
+                            n++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 拟合碰撞体,自动根据身上的Mesh组件生成对应的Collier
+    /// </summary>
+    public class MeshCollider : Collider
+    {
+        /// <summary>
+        /// 保存着碰撞体信息,初始化之后可以选择再进行更改,暂时不支持其他种类的Collider;
+        /// </summary>
+        public List<Vector2Int> posList;
+        public bool Init()
+        {
+            Renderer r = GetComponent<Renderer>();
+            if (r == null)
+            {
+                Debug.Warning(gameObject.Name + "对象没有Renderer组件");
+                return false;
+            }
+            posList = new List<Vector2Int>();
+            //添加当前点
+            if (r.GetType() == typeof(PosRenderer))
+            {
+                posList.Add(new Vector2Int(0, 0));
+            }
+            //添加一排碰撞体
+            else if (r.GetType() == typeof(StringRenderer))
+            {
+                int n = 0;
+                for (int i = 0; i < ((StringRenderer)r).length; i += Camera.main.CharWidth)
+                {
+                    posList.Add(new Vector2Int(n, 0));
+                    n++;
+                }
+            }
+            //添加组碰撞体
+            else if (r.GetType() == typeof(GroupRenderer))
+            {
+                GroupRenderer gr = r as GroupRenderer;
+                foreach (var v in gr.list)
+                {
+                    if (v.Key.GetType() == typeof(PosRenderer))
+                    {
+                        posList.Add(v.Value);
+                    }
+                    else if (v.Key.GetType() == typeof(StringRenderer))
+                    {
+                        int n = 0;
+                        for (int i = 0; i < ((StringRenderer)v.Key).length; i += Camera.main.CharWidth)
+                        {
+                            posList.Add(new Vector2Int(n, 0) + v.Value);
+                            n++;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        public MeshCollider()
+        {
+            
+        }
+
+    }
+
+    public class RigidBody : Component
+    {
+        //刚体的质量,可以决定碰撞.
+        public float Mass = 1;
+        public Vector2 FPosition;
+        public Vector2 MoveSpeed;
+        public RigidBody()
+        {
+            Mass = 1;
+            FPosition = Vector2.Zero;
+            MoveSpeed = Vector2.Zero;
+        }
+
+        /// <summary>
+        /// 设置速度
+        /// </summary>
+        public void SetSpeed(Vector2 vector)
+        {
+            MoveSpeed = vector;
+        }
+        /// <summary>
+        /// 添加一个持续的力的作用.待更新其他模式
+        /// </summary>
+        internal void AddForce(Vector2 vector)
+        {
+            MoveSpeed += vector;
+        }
+
+
+        /// <summary>
+        /// 停止移动并清除偏移浮点缓存
+        /// </summary>
+        public void Stop()
+        {
+            FPosition = Vector2.Zero;
+            MoveSpeed = Vector2.Zero;
+        }
+
+        /// <summary>
+        /// 向movespeed方向移动一帧 进行内部的浮点运动,返回移动的坐标结果
+        /// </summary>
+        internal Vector2Int Move()
+        {
+            FPosition += MoveSpeed * Time.DeltaTime;
+            int dx = 0, dy = 0;
+            if(FPosition.X > 1)
+            {
+                FPosition.X -= 1;
+                dx += 1;
+            }
+            else if(FPosition.X < -1)
+            {
+                FPosition.X += 1;
+                dx -= 1;
+            }
+
+            if (FPosition.Y > 1)
+            {
+                FPosition.Y -= 1;
+                dy += 1;
+            }
+            else if (FPosition.Y < -1)
+            {
+                FPosition.Y += 1;
+                dy -= 1;
+            }
+
+            return new Vector2Int(dx, dy);
+        }
+
     }
 }
--- Destroy/Core/Systems/PhysicsSystem.cs
+++ Destroy/Core/Systems/PhysicsSystem.cs
@@ -4,8 +4,132 @@
 
     internal static class PhysicsSystem
     {
+
+        public static Dictionary<Vector2Int, Collider> staticColliders = new Dictionary<Vector2Int, Collider>();
+        /// <summary>
+        /// 初始化,将静态碰撞体加入静态对象中
+        /// </summary>
+        public static void Init(List<GameObject> gameObjects)
+        {
+            foreach(var v in gameObjects)
+            {
+                StaticCollider staticCollider = v.GetComponent<StaticCollider>();
+                if (staticCollider != null)
+                {
+                    foreach(Vector2Int dis in staticCollider.posList)
+                    {
+                        Vector2Int vi = staticCollider.transform.Position + dis;
+                        if (!staticColliders.ContainsKey(vi))
+                            staticColliders.Add(vi, staticCollider);
+                    }
+                }
+            }
+        }
+
+        private static bool HasInit = false;
+
+        //Update流程 写入静态碰撞体 - 写入动态碰撞体 - 检出rigidbody - 移动rigidbody - 循环检测rigidbody和collider的碰撞
         public static void Update(List<GameObject> gameObjects)
         {
+            if(!HasInit)
+            {
+                PhysicsSystem.Init(gameObjects);
+                PhysicsSystem.HasInit = true;
+                return;
+            }
+
+            Dictionary<Vector2Int, Collider> colliders = new Dictionary<Vector2Int, Collider>();
+            List<RigidBody> rigids = new List<RigidBody>();
+
+            foreach (var gameObject in gameObjects)
+            {
+                //将动态碰撞体加入colliders字典
+                MeshCollider mc  = gameObject.GetComponent<MeshCollider>();
+                if (mc!=null)
+                {
+                    foreach(Vector2Int dis in mc.posList)
+                    {
+                        Vector2Int vi = gameObject.transform.Position + dis;
+                        if (!colliders.ContainsKey(vi))
+                            colliders.Add(vi, mc);
+                    }
+                }
+                //检出所有rigid准备进行处理
+                RigidBody rigid = gameObject.GetComponent<RigidBody>();
+                if (rigid != null)
+                {
+                    rigids.Add(rigid);
+                }
+            }
+
+            foreach(var rigid in rigids)
+            {
+                //当前刚体要移动的目标点
+                Vector2Int dis = rigid.Move();
+                Vector2Int to = rigid.transform.Position + dis;
+                //当产生了位移之后,开始检测移动系统
+                if (dis != Vector2Int.Zero)
+                { 
+                    //撞墙了,强制这个rigid停止移动
+                    if (staticColliders.ContainsKey(to))
+                    {
+                        RuntimeEngine.CallScriptMethod(rigid.gameObject, "OnCollision", false, staticColliders[to]);
+                        rigid.Stop();
+                        return;
+                    }
+                    //如果撞到了其他活动的colliders
+                    else if (colliders.ContainsKey(to))
+                    {
+                        Collider otherCollider = colliders[to];
+                        //如果这个是自己,那么正常移动并返回
+                        if (otherCollider == rigid.GetComponent<MeshCollider>())
+                        {
+                            rigid.transform.Translate(dis);
+                            return;
+                        }
+                        //获得自己的质量
+                        float thisMass = rigid.Mass;
+                        //获得对方的质量
+                        float otherMass;
+                        RigidBody otherRigid = otherCollider.GetComponent<RigidBody>();
+                        if (otherRigid != null)
+                        {
+                            otherMass = otherRigid.Mass;
+
+                            //如果自己的质量比对方小,那么自己被阻挡停止
+                            if (thisMass <= otherMass)
+                            {
+                                RuntimeEngine.CallScriptMethod(rigid.gameObject, "OnCollision", false, colliders[to]);
+                                rigid.Stop();
+                                return;
+                            }
+                            //如果自己质量比对方大,那么将对方推走,下一帧再自己走
+                            else
+                            {
+                                RuntimeEngine.CallScriptMethod(rigid.gameObject, "OnCollision", false, colliders[to]);
+                                otherRigid.transform.Translate(dis);
+                                rigid.transform.Translate(dis);
+                                //rigid.AddFPosition(new Vector2(dis.X, dis.Y));
+                                return;
+                            }
+                        }
+                        //如果没有获取对方的质量,那么强制停止
+                        else
+                        {
+                            RuntimeEngine.CallScriptMethod(rigid.gameObject, "OnCollision", false, colliders[to]);
+                            rigid.Stop();
+                            return;
+                        }
+
+                    }
+                    //没有撞墙,那么移动transform
+                    else
+                    {
+                        rigid.transform.Translate(dis);
+                    }
+                }
+            }
+            /*
             for (int i = 0; i < gameObjects.Count; i++)
             {
                 GameObject gameObject = gameObjects[i];
@@ -35,6 +159,7 @@
                         RuntimeEngine.CallScriptMethod(gameObject, "OnCollision", false, otherCollider);
                 }
             }
+            */
         }
     }
 }
